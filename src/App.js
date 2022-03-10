87a8f1843ed11b9ada56a9d71f63ac3f7d4661bc
Dheerendra Singh <dheerendrasingh12@gmail.com>
2022-03-11 01:31:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,11 +6,16 @@ function App() {
   return (
     <div className="App"     >
       <header className="App-image">
-        <p>
-        </p>
-    <h2 style={{ color: 'yellow' }}>      I am getting married on the 21 of April 2022. I would like to invite all of you to be a part of the celebrations and will send you the details of the wedding shortly.
-  </h2> 
-     </header>
+
+        <text>
+          <div>    <h2 style={{ color: 'yellow' }}>      I am getting married on the 21 of April 2022.
+          </h2>
+          <br></br>
+            <div style={{ color: 'blue' }}> I would like to invite all of you to be a part of the celebrations and will send you the details of the wedding shortly.</div>
+
+          </div>
+        </text>
+      </header>
     </div>
   );
 }
