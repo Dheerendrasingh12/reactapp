25d100aa401a4f0e01db9fcb8946b67e6bd38182
Dheerendra Singh <dheerendrasingh12@gmail.com>
2022-03-11 00:34:28 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Image from "./image/image.JPG";
+
+
+function App() {
+  return (
+    <div className="App"     >
+      <header className="App-image">
+        <p>
+        </p>
+    <h2 style={{ color: 'grey' }}>      I am getting married on the 21 of April 2022. I would like to invite all of you to be a part of the celebrations and will send you the details of the wedding shortly.
+  </h2> 
+     </header>
+    </div>
+  );
+}
+
+export default App;
