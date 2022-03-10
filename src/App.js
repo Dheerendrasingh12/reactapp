6c63b83ced20a78b050958ad18a83c119c1e0d2b
Dheerendra Singh <dheerendrasingh12@gmail.com>
2022-03-11 01:17:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Image from "./image/image.JPG";
 
 
 function App() {
@@ -9,7 +8,7 @@ function App() {
       <header className="App-image">
         <p>
         </p>
-    <h2 style={{ color: 'grey' }}>      I am getting married on the 21 of April 2022. I would like to invite all of you to be a part of the celebrations and will send you the details of the wedding shortly.
+    <h2 style={{ color: 'yellow' }}>      I am getting married on the 21 of April 2022. I would like to invite all of you to be a part of the celebrations and will send you the details of the wedding shortly.
   </h2> 
      </header>
     </div>
